c6d43d4fc3e242063bccf7b8c2ae331cdfbba240
Mostafahakki <Mostafa.hakki@hotmail.com>
2023-06-20 19:04:42 -0700

    nestedloops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 {
         if (n > 0)
         {
-                _putchar('+');
+                _putchar(43);
                 return (1);
         }
         else if (n == 0)
@@ -19,10 +19,9 @@ int print_sign(int n)
                 _putchar(48);
                 return (0);
         }
-        else if (n < 0)
+        else
         {
-                _putchar('-');
-
+                _putchar(45);
                 return (-1);
         }
 }
