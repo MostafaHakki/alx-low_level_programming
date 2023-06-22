20be1496f8906151aa2e4df6cd66d0539938e7ac
Mostafahakki <Mostafa.hakki@hotmail.com>
2023-06-22 08:29:46 -0700

    morefuntions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main:check if c is uppercase
+ *
+ *@c:input for alphabet
+ *
+ * Return: 1 if uppercase 0 if not
+ */
+
+int _isupper(int c);
+{
+	if(c >= 65 && c <=90)
+		Return(1);
+	else
+		Return(0);
+}
